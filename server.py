9906251a7c778b4e6ce8b4bfab20126f274d2c75
Eathonlin <41421772+Eathonlin@users.noreply.github.com>
2019-05-05 16:33:23 +0800

    update

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import VideoCamera as Video
 
 app = Flask(__name__)
 
-
+# 登入頁  檢查帳號密碼
 @app.route("/", methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -14,7 +14,7 @@ def login():
             return render_template("index.html", alertdata="密碼錯誤!!")
     return render_template("index.html")
 
-
+#首頁菜單 URL連結
 @app.route('/myside/<myurl>')
 def Leaflet(myurl):
     return render_template(myurl + '.html')
@@ -34,4 +34,4 @@ def image():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
